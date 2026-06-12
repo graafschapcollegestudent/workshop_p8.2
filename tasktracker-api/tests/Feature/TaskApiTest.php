b1940da1a7d1d820e10cdad6f203decde04eabb4
Hadi <167310042+graafschapcollegestudent@users.noreply.github.com>
2026-06-12 14:45:17 +0200

    break test for CI assignment

--- tasktracker-api/tests/Feature/TaskApiTest.php
+++ tasktracker-api/tests/Feature/TaskApiTest.php
@@ -17,7 +17,7 @@ class TaskApiTest extends TestCase
         $response = $this->getJson('/api/tasks');
 
         $response->assertStatus(200)
-                 ->assertJsonCount(3);
+                 ->assertJsonCount(999);
     }
 
     public function test_can_create_a_task()
